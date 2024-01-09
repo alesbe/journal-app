49296c2d5a1332a287fba625a4909bb640199a00
alesbe <aesparzabellver@gmail.com>
2024-01-09 15:44:10 +0100

    added routers to main router

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage, RegisterPage } from "../pages"
 
-const AuthRoutes = () => {
+export const AuthRoutes = () => {
   return (
     <Routes>
         <Route path="login" element={ <LoginPage/> } />
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom"
+import { AuthRoutes } from "../auth/routes/AuthRoutes"
+import { JournalRoutes } from "../journal/routes/JournalRoutes"
 
 export const AppRouter = () => {
   return (
     <Routes>
+
         {/* Login y registro */}
-        <Route />
+        <Route path="/auth/*" element={ <AuthRoutes/>  } />
 
         {/* JournalApp */}
-        <Route />
+        <Route path="/*" element={ <JournalRoutes/>  } />
+        
     </Routes>
   )
 }
