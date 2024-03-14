564c1f596ca2d2109aa37fa06ba7307c37eebe29
alesbe <aesparzabellver@gmail.com>
2024-03-14 11:46:41 +0100

    added navbar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material"
+import { NavBar } from "../components/NavBar";
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx = {{ display: 'flex' }}>
-        {/*navbar drawerWidth*/}
+
+        <NavBar drawerWidth={ drawerWidth }/>
 
         {/*sidebar drawerWidth*/}
 
