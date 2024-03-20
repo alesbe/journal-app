39652b3aaf229fb86e7bfd41fd34eef9b87869da
alesbe <aesparzabellver@gmail.com>
2024-03-20 16:03:31 +0100

    added nothing selected view and create button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,28 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { AddOutlined, MailOutline } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis volutpat lectus dui, et cursus nunc imperdiet et.</Typography>
+      <NothingSelectedView />
+      
+      {/* <NoteView/> */}
 
-      {/* nothing selected */}
-      {/* note view */}
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined src={{ fontSize: 30 }}/>
+      </IconButton>
     </JournalLayout>
   )
 }
