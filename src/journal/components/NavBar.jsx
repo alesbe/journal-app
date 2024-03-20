8b7a3dca5f77cc1829a7ee6079592f4bb3c9a294
alesbe <aesparzabellver@gmail.com>
2024-03-20 16:02:06 +0100

    added navbar

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -3,30 +3,32 @@ import { AppBar, Grid, IconButton, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 export const NavBar = ({ drawerWidth = 240 }) => {
-  return (
-    <AppBar
-        position='fixed'
-        sx={{ 
-            width: { sm: `calc(100% - ${ drawerWidth }px)` },
-            ml: {sm: `${drawerWidth}px`}
-        }}>
+    return (
+        <AppBar 
+            position='fixed'
+            sx={{ 
+                width: { sm: `calc(100% - ${ drawerWidth }px)` },
+                ml: { sm: `${ drawerWidth }px` }
+             }}
+        >
             <Toolbar>
                 <IconButton
-                    color = 'inherit'
-                    edge = "start"
-                    sx = {{ mr: 2, display: { sm: 'none' } }}
+                    color='inherit'
+                    edge="start"
+                    sx={{ mr: 2, display: { sm: 'none' } }}
                 >
-                    <MenuOutlined/>
+                    <MenuOutlined />
                 </IconButton>
 
                 <Grid container direction='row' justifyContent='space-between' alignItems='center'>
-                    <Typography variant='h6' noWrap component="div">JournalApp</Typography>
+                    <Typography variant='h6' noWrap component='div'>JournalApp</Typography>
 
-                    <IconButton color="error">
-                        <LogoutOutlined/>
+                    <IconButton color='error'>
+                        <LogoutOutlined />
                     </IconButton>
                 </Grid>
+
             </Toolbar>
-    </AppBar>
-  )
+        </AppBar>
+    )
 }
