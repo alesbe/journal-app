03df5d3871213c148fc584829278ac776a5371f3
alesbe <aesparzabellver@gmail.com>
2024-04-02 19:54:49 +0200

    added auth slice

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+    name:'auth',
+    initialState: {
+        status: 'checking', // 'not-authenticated', 'authenticated'
+        uid: null,
+        email: null,
+        displayName: null,
+        photoURL: null,
+        errorMessage: null,
+    },
+    reducers: {
+        login: (state, action) => {
+
+        },
+        logout: (state, payload) => {
+
+        },
+        checkingCredentials: (state) => {
+
+        }
+    }
+});
+
+export const { login, logout, checkingCredentials } = authSlice.actions;
