7dcf64d772f8cebd024f63c98c145e8efb9a6758
alesbe <aesparzabellver@gmail.com>
2024-04-13 15:13:45 +0200

    added registerUserWithEmailPassword() and documentation

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,8 +1,13 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup, updateProfile } from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
 
+/**
+ * Inicia sesión en Firebase con Google. Abre un popup para que se loguee.
+ * 
+ * @returns Datos del usuario o error
+ */
 export const signInWithGoogle = async() => {
     try {
         const result = await signInWithPopup(FirebaseAuth, googleProvider);
@@ -19,6 +24,35 @@ export const signInWithGoogle = async() => {
         return {
             ok: false,
             errorMessage
+        }
+    }
+}
+
+/**
+ * Crea un usuario en Firebase con usuario y contraseña.
+ * 
+ * @param Datos del usuario
+ * @returns Datos del usuario o error
+ */
+export const registerUserWithEmailPassword = async ({ email, password, displayName }) => {
+    try {
+        // Creamos el usuario en firebase
+        const res = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
+        const{ uid, photoURL } = res.user;
+
+        // Actualizamos el usuario en Firebase con el nombre de usuario que ha introducido el usuario
+        await updateProfile( FirebaseAuth.currentUser, { displayName } );
+
+        return {
+            ok: true,
+            uid, photoURL, email, displayName
+        }
+    } catch (error) {
+        console.log(error);
+
+        return {
+            ok: false,
+            errorMessage: error.message
         }
     }
 }
