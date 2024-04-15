c44399468d223645f4201c0309568c046dc9318c
alesbe <aesparzabellver@gmail.com>
2024-04-15 16:21:38 +0200

    added displayName to sidebar

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,49 +1,53 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+import { useSelector } from "react-redux"
 
 export const SideBar = ({ drawerWidth = 240 }) => {
-  return (
-    <Box
-        component='nav'
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-    >
-        <Drawer
-            variant="permanent"
-            open={ true }
-            sx={{
-                display: { xs: 'block' },
-                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth } // Esto modifica la clase propia de MaterialUI
-            }}
+
+    const { displayName } = useSelector(state => state.auth);
+
+    return (
+        <Box
+            component='nav'
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         >
-            <Toolbar>
-                <Typography variant='h6' noWrap component='div'>
-                    Álvaro Esparza
-                </Typography>
-            </Toolbar>
+            <Drawer
+                variant="permanent"
+                open={ true }
+                sx={{
+                    display: { xs: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth } // Esto modifica la clase propia de MaterialUI
+                }}
+            >
+                <Toolbar>
+                    <Typography variant='h6' noWrap component='div'>
+                        { displayName }
+                    </Typography>
+                </Toolbar>
 
-            <Divider />
+                <Divider />
 
-            <List>
-                {
-                    ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'].map( text => (
-                        <ListItem key = { text } disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot />
-                                </ListItemIcon>
+                <List>
+                    {
+                        ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'].map( text => (
+                            <ListItem key = { text } disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <TurnedInNot />
+                                    </ListItemIcon>
 
-                                <Grid container>
-                                    <ListItemText primary={ text } />
-                                    <ListItemText secondary={ 'Esto es un texto de prueba. Aquí irá información adicional.' } />
-                                </Grid>
+                                    <Grid container>
+                                        <ListItemText primary={ text } />
+                                        <ListItemText secondary={ 'Esto es un texto de prueba. Aquí irá información adicional.' } />
+                                    </Grid>
 
-                            </ListItemButton>
-                        </ListItem>
-                    ) )
-                }
-            </List>
+                                </ListItemButton>
+                            </ListItem>
+                        ) )
+                    }
+                </List>
 
-        </Drawer>
-    </Box>
-  )
+            </Drawer>
+        </Box>
+    )
 }
